fb2ed0b13dd2a0fab3f7bb161c5c26a414dafc33
abdulwassayansari-001 <wassay.ansari@xloopdigital.com>
2023-10-14 13:14:56 +0500

    increased model data max_length

--- data/models.py
+++ data/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 
 class DataScrap(models.Model):
-    name = models.CharField(max_length=255, null=True)
-    dep = models.CharField(max_length=255, blank=True, null=True)
-    designation = models.CharField(max_length=255, blank=True, null=True)
-    address = models.CharField(max_length=255, blank=True, null=True)
-    email = models.EmailField(max_length=255, blank=True, null=True)
-    phone_number = models.CharField(max_length=20, blank=True, null=True)
-    link = models.CharField(max_length=255, blank=True, null=True)
+    name = models.CharField(max_length=512, null=True)
+    dep = models.CharField(max_length=512, blank=True, null=True)
+    designation = models.CharField(max_length=512, blank=True, null=True)
+    address = models.CharField(max_length=512, blank=True, null=True)
+    email = models.EmailField(max_length=512, blank=True, null=True)
+    phone_number = models.CharField(max_length=256, blank=True, null=True)
+    link = models.CharField(max_length=512, blank=True, null=True)
     desc = models.TextField(blank=True, null=True)
-    hierarchy = models.CharField(max_length=255, blank=True, null=True)
+    hierarchy = models.CharField(max_length=512, blank=True, null=True)
     image_name = models.TextField(blank=True, null=True)
     validation = models.BooleanField(default=False, null=True)
 
