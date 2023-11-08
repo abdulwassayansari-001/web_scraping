0567a8daabca63a366edc94459846e45baadf30c
abdulwassayansari-001 <wassay.ansari@xloopdigital.com>
2023-11-08 13:25:39 +0500

    configured settings.py

--- web_scraping/settings.py
+++ web_scraping/settings.py
@@ -15,10 +15,6 @@ import os
 from pathlib import Path
 
 import environ
-from django.conf import global_settings
-from google.cloud import secretmanager
-
-from google.oauth2 import service_account
 
 PROJECT_PACKAGE = Path(__file__).resolve().parent
 
@@ -28,29 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env(DEBUG=(bool, False))
 env_file = os.path.join(BASE_DIR, ".env")
 
-if os.environ.get("GOOGLE_CLOUD_PROJECT", None):
-    # Pull secrets from Secret Manager
-    project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
-
-    client = secretmanager.SecretManagerServiceClient()
-    settings_name = os.environ.get("SETTINGS_NAME", "web_scraping_app")
-    name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
-    payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
-
-    env.read_env(io.StringIO(payload))
-elif os.path.isfile(env_file):
-    # Use a local secret file, if provided
-    env.read_env(env_file)
-else:
-    raise Exception("No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = 'secret_key'
 
-DEBUG = env("DEBUG")
+DEBUG = True
 
 if DEBUG:
     STATICFILES_DIRS = [str(PROJECT_PACKAGE.joinpath("static"))]
@@ -109,14 +90,14 @@ WSGI_APPLICATION = "web_scraping.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {"default": env.db()}
+# DATABASES = {"default": env.db()}
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # If the flag as been set, configure to use proxy
 if os.getenv("USE_CLOUD_SQL_AUTH_PROXY", None):
@@ -170,22 +151,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Django-Registration
 ACCOUNT_ACTIVATION_DAYS = 7
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
-CRISPY_TEMPLATE_PACK = "bootstrap5"
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
-
-if os.getenv("GOOGLE_CLOUD_PROJECT"):
-    DEFAULT_FILE_STORAGE = "storages.backends.gcloud.GoogleCloudStorage"
-    STATICFILES_STORAGE = "storages.backends.gcloud.GoogleCloudStorage"
-    GS_BUCKET_NAME = os.environ.get("GS_BUCKET_NAME")
-    GS_DEFAULT_ACL = "publicRead"
-    MEDIA_URL = f"https://storage.googleapis.com/{os.environ.get('GS_BUCKET_NAME')}/"
-else:
-    # Serve static and media files locally during development
-    STATICFILES_DIRS = [str(PROJECT_PACKAGE.joinpath("static"))]
-    STATIC_URL = "/static/"
-    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-    MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
