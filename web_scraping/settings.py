59b7c69a1da136b4cb49f474553030c3eb97851e
abdulwassayansari-001 <wassay.ansari@xloopdigital.com>
2023-11-15 12:10:13 +0500

    Env variables configuration

--- web_scraping/settings.py
+++ web_scraping/settings.py
@@ -23,15 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env(DEBUG=(bool, False))
 env_file = os.path.join(BASE_DIR, ".env")
-
+env.read_env(env_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'secret_key'
+SECRET_KEY = env("SECRET_KEY")
 
-DEBUG = True
+DEBUG = env("DEBUG")
 
 if DEBUG:
     STATICFILES_DIRS = [str(PROJECT_PACKAGE.joinpath("static"))]
@@ -186,11 +186,14 @@ LOGIN_REDIRECT_URL = "/"
 # EMAIL_HOST_PASSWORD=""
 # EMAIL_USE_TLS=False
 
-EMAIL_HOST='smtp.sendgrid.net'
-EMAIL_PORT=587
-EMAIL_HOST_USER='apikey'
-EMAIL_HOST_PASSWORD='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'
-DEFAULT_FROM_EMAIL='abdulwassay.cosmic@gmail.com'
+# E-mail
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS") == "True"
+# DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
 
 # ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 
